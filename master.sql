6ea95dafbb74e89bcd9978d00b81cd7e24aacf0f
maxim-lixakov <mvliksako1@mts.ru>
2023-11-06 14:17:55 +0300

    add slave_1, slave_2, slave_3

--- master.sql
+++ master.sql
@@ -1,3 +1,3 @@
 CREATE USER repl@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT REPLICATION SLAVE ON *.* TO repl@'%';
-CHANGE MASTER TO MASTER_HOST='mysql-slave', MASTER_USER='replslave', MASTER_PASSWORD='password';
+# CHANGE MASTER TO MASTER_HOST='mysql-slave-1', MASTER_USER='replslave', MASTER_PASSWORD='password';
