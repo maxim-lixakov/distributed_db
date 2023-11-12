ce036b7d39596a7dcf7c6accfd92ca89e7feb618
maxim-lixakov <mvliksako1@mts.ru>
2023-11-12 23:09:36 +0300

    update admin panel, add prod configuration

--- app/main/admin.py
+++ app/main/admin.py
@@ -2,16 +2,23 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Cinema)
-admin.site.register(Country)
-admin.site.register(Customer)
-admin.site.register(Film)
-admin.site.register(FilmGenre)
-admin.site.register(Genre)
-admin.site.register(Hall)
-admin.site.register(ProductionCountry)
-admin.site.register(Seat)
-admin.site.register(Seller)
-admin.site.register(Showtime)
-admin.site.register(Ticket)
 
+class ModelAdmin(admin.ModelAdmin):
+
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields]
+        super(ModelAdmin, self).__init__(model, admin_site)
+
+
+admin.site.register(Cinema, ModelAdmin)
+admin.site.register(Country, ModelAdmin)
+admin.site.register(Customer, ModelAdmin)
+admin.site.register(Film, ModelAdmin)
+admin.site.register(FilmGenre, ModelAdmin)
+admin.site.register(Genre, ModelAdmin)
+admin.site.register(Hall, ModelAdmin)
+admin.site.register(ProductionCountry, ModelAdmin)
+admin.site.register(Seat, ModelAdmin)
+admin.site.register(Seller, ModelAdmin)
+admin.site.register(Showtime, ModelAdmin)
+admin.site.register(Ticket, ModelAdmin)
